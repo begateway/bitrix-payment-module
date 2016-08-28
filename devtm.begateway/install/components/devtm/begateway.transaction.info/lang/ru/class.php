08944712f389f9b7b525420b3311db675348e4d1
avsh <avterus@gmail.com>
2016-08-28 11:45:23 +0300

    don't check token length

--- devtm.begateway/install/components/devtm/begateway.transaction.info/lang/ru/class.php
+++ devtm.begateway/install/components/devtm/begateway.transaction.info/lang/ru/class.php
@@ -1,7 +1,6 @@
 <?
 $MESS["DEVTM_BEGATEWAY_NO_TRANS_INFO"]     = "Информация по транзакции не может быть получена";
 $MESS["DEVTM_BEGATEWAY_NO_TOKEN_ACCESS"]   = "Токен не пренадлежит данному пользователю";
-$MESS["DEVTM_BEGATEWAY_WRONG_TOKEN_LONG"]  = "Токен неверной длины";
 $MESS["DEVTM_BEGATEWAY_FAIL_TOKEN_QUERY"]  = "Ошибка получения информации по транзакции";
 $MESS["DEVTM_BEGATEWAY_WRONG_ORDER_ID"]    = "Неизвестный идентификатор заказа";
 $MESS["DEVTM_BEGATEWAY_WRONG_PAYMENT_ID"]  = "Неизвестный идентификатор оплаты";
