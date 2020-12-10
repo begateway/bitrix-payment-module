e1ece91904ad630a5b487354f5ff46d2d478afb0
avsh <avsh@users.noreply.github.com>
2020-12-10 17:40:32 +0300

    fix path for .settings.php inside container

--- docker/configs/.settings.php
+++ docker/configs/.settings.php
@@ -29,11 +29,11 @@ return [
     'value' =>
     [
       'debug' => true,
-      'handled_errors_types' => E_ALL,
-      'exception_errors_types' => E_ALL,
+      'handled_errors_types' => 4437,
+      'exception_errors_types' => 4437,
       'ignore_silence' => false,
       'assertion_throws_exception' => true,
-      'assertion_error_type' => E_ALL,
+      'assertion_error_type' => 256,
       'log' => NULL,
     ],
     'readonly' => false,
